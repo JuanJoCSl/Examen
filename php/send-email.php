e392f7622a975c7de04baa47dec9a7bfe8dbbaaf
juan jose condori salazar <juanjokun2002@gmail.com>
2025-03-25 22:05:30 -0400

    segundo commit agregamos pagina de agradecimiento

--- php/send-email.php
+++ php/send-email.php
@@ -30,9 +30,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Send the email
     if (mail($recipient, $subject, $email_content, $email_headers)) {
-        // Set a 200 (okay) response code
-        http_response_code(200);
-        echo "¡Gracias! Tu mensaje ha sido enviado.";
+        // Redirect to thank you page
+        header("Location: ../gracias.html");
+        exit;
     } else {
         // Set a 500 (internal server error) response code
         http_response_code(500);
